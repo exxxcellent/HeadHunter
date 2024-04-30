ce5c6254a2c30fd557a93368535070936cd2864a
exxxcellent <aleksejdikun@mail.ru>
2024-04-30 12:25:33 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App(): JSX.Element {
+    return (
+        <div className="container">
+            Начинаем создавать hh.ru
+        </div>
+    )
+}
+
+export default App
---
+++ src/pages/Main.tsx
@@ -0,0 +1,12 @@
+
+function Main(): JSX.Element {
+    return (
+        <>
+            <header>
+
+            </header>
+        </>
+    )
+}
+
+export default Main
